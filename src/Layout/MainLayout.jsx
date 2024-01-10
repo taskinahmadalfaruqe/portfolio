36303d274b6a672e4400ec188f02a5c8b4fa220d
TASKIN AHMAD AL FARUQE <119452412+taskinahmadalfaruqe@users.noreply.github.com>
2024-01-10 22:30:58 +0600

    Add A Annimation

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -32,6 +32,7 @@ const MainLayout = () => {
                     {/* MAIN NAVBAR  */}
                     <Navbar></Navbar>
 
+                    
                     {/* ALL CHILDREAN  */}
                     <Outlet></Outlet>
                     <Footer></Footer>
